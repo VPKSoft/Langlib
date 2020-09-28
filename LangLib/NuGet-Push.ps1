eb594e262229bfba1d39114215fd789fcac4034a
VPKSoft <vpksoft@vpksoft.net>
2020-09-28 16:44:26 +0300

    Add NuGet push rev. 1.

---
+++ LangLib/NuGet-Push.ps1
@@ -0,0 +1,33 @@
+﻿
+Write-Output "Init NuGet push..."
+
+$output_file = "CryptEnvVar.exe"
+
+$download_url = "https://www.vpksoft.net/toolset/CryptEnvVar.exe"
+
+Write-Output "Download file:  $download_url ..."
+(New-Object System.Net.WebClient).DownloadFile($download_url, $output_file)
+Write-Output "Download done."
+
+# create the digital signature..
+CryptEnvVar -s $Env:SECRET_KEY -e "CERT_1;CERT_2;CERT_3" > vpksoft.pfx
+
+# sign and push the NuGet packages..
+$files = dir -r -Name -Filter *LangLibMultiDB*.nupkg # use the mask to discard possible third party packages..
+for ($i = 0; $i -lt $files.Count; $i++) 
+{ 
+    # sign the NuGet packages.
+	Write-Output ("Signing package:" + $files[$i] + " ...")
+    nuget.exe sign $files[$i] -CertificatePath "vpksoft.pfx" -Timestamper "http://timestamp.comodoca.com" -CertificatePassword $Env:PFX_PASS > null 2>&1
+	Write-Output ("Package signed: " + $files[$i] + ".")
+
+    # push the NuGet packges..
+    #$nuget_api = "https://api.nuget.org/v3/index.json"
+
+    $nuget_api = "https://apiint.nugettest.org/v3/index.json"
+	Write-Output ("Pushing NuGet:" + $files[$i] + " ...")
+    nuget.exe push $files[$i] $Env:NUGET_TEST_APIKEY -Source $nuget_api -SkipDuplicate
+	Write-Output ("Pushing done:" + $files[$i] + ".")
+}
+
+Write-Output "NuGet push finished."
