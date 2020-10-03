b54715abd645b64c20c2f634dc661801eab78b9f
VPKSoft <vpksoft@vpksoft.net>
2020-10-03 10:15:18 +0300

    Push script fixed, rev.2?

--- LangLib/NuGet-Push.ps1
+++ LangLib/NuGet-Push.ps1
@@ -19,23 +19,25 @@ $args = @("-s", $Env:SECRET_KEY, "e", "CERT_1;CERT_2;CERT_3", "-f", "vpksoft.pfx
 $files = dir -r -Name -Filter *.nupkg # use the mask to discard possible third party packages..
 for ($i = 0; $i -lt $files.Count; $i++) 
 { 
-    # sign the NuGet packages.
-	Write-Output ("Signing package:" + $files[$i] + " ...")
+    $file = $files[$i]
 
-    $args = @("sign", $files[$i], "-CertificatePath", "vpksoft.pfx", "-Timestamper", "http://timestamp.comodoca.com", "-CertificatePassword", "$Env:PFX_PASS")
+    # sign the NuGet packages.
+	Write-Output (-join("Signing package: ", $file, " ..."))
+
+    $args = @("sign", $file, "-CertificatePath", "vpksoft.pfx", "-Timestamper", "http://timestamp.comodoca.com", "-CertificatePassword", $Env:PFX_PASS)
 
     nuget.exe $args > null 2>&1
-	Write-Output @("Package signed: ", $files[$i], ".")
+	Write-Output (-join("Package signed: ", $file, "."))
 
     # push the NuGet packges..
     #$nuget_api = "https://api.nuget.org/v3/index.json"
 
     $nuget_api = "https://apiint.nugettest.org/v3/index.json"
-	Write-Output @("Pushing NuGet:", $files[$i], " ...")
+	Write-Output (-join("Pushing NuGet:", $file, " ..."))
 
-    $args = @("push", $files[$i], $Env:NUGET_TEST_APIKEY, "-Source", $nuget_api, "-SkipDuplicate")
+    $args = @("push", $file, $Env:NUGET_TEST_APIKEY, "-Source", $nuget_api, "-SkipDuplicate")
     nuget.exe $args
-	Write-Output @("Pushing done:", $files[$i], ".")
+	Write-Output (-join("Pushing done:", $file, "."))
 }
 
 Write-Output "NuGet push finished."
